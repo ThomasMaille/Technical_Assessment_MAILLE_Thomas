769442e87ebe45a1545b6c1d12db8b971177878f
ThomasMaille <thomas.maille13@gmail.com>
2025-09-09 15:54:53 +0200

    feat(ajax_clienta): mise en forme des données à afficher pour le client a

--- customs/clienta/modules/cars/ajax.php
+++ customs/clienta/modules/cars/ajax.php
@@ -1,1 +1,17 @@
 <h1>Voitures Client A</h1>
+
+<?php  
+
+$cars = json_decode(file_get_contents('../../../../data/cars.json'), true);
+
+foreach($cars as $car){
+    if($car["customer"] === 'clienta'){
+        echo "<li>";
+        echo "<strong>" . $car["modelName"] . "</strong> - " . $car["brand"];
+        echo " | Année : " . date("Y", $car["year"]);
+        echo " | Puissance : " . $car["power"] . " ch";
+        echo "</li>";
+    }
+}
+
+?>
