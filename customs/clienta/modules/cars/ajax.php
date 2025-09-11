ff6a2ebcd08a3bdebe7aab6e8318e3a4425709b3
ThomasMaille <thomas.maille13@gmail.com>
2025-09-11 12:52:33 +0200

    feat(cars_clienta): ajout de couleurs pour repérer les voitures selon l'année pour le client a, ajout de voitures dans cars.json

--- customs/clienta/modules/cars/ajax.php
+++ customs/clienta/modules/cars/ajax.php
@@ -6,7 +6,18 @@ $cars = json_decode(file_get_contents('../../../../data/cars.json'), true);
 
 foreach($cars as $car){
     if($car["customer"] === 'clienta'){
-        echo "<li class='car-item' data-id='{$car["id"]}'>";
+        
+        $carAge = (int)date("Y") - (int)date("Y", $car["year"]);
+
+        if ($carAge > 10) {
+            $couleur = "red";
+        } elseif ($carAge < 2) {
+            $couleur = "green";
+        } else {
+            $couleur = "black";
+        }
+
+        echo "<li style='color: {$couleur}' class='car-item' data-id='{$car["id"]}'>";
         echo "<strong>" . $car["modelName"] . "</strong> - " . $car["brand"];
         echo " | Année : " . date("Y", $car["year"]);
         echo " | Puissance : " . $car["power"] . " ch";
